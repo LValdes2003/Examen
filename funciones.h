489045bba98d2bcfae021c0c63655251b960f1a1
LValdes2003 <lvaldesp@myuax.com>
2023-11-07 11:10:23 +0100

    Empezar otra vez

--- funciones.h
+++ funciones.h
@@ -1,6 +1,4 @@
 #ifndef EXAMEN_FUNCIONES_H
 #define EXAMEN_FUNCIONES_H
 
-int triplesPitagoricos(int triple[]);
-
 #endif //EXAMEN_FUNCIONES_H
--- main.cpp
+++ main.cpp
@@ -2,8 +2,5 @@
 #include "funciones.h"
 
 int main() {
-    int triple[3];
-    triplesPitagoricos(triple);
-    std::cout << triple[0] << " " << triple[1] << " " << triple[2] << std::endl;
     return 0;
 }
